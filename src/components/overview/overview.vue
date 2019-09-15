6f5a2a2b797abedfa5b862109076435ca930d723
Vbobell <vinicius.bobel41@gmail.com>
2019-09-14 21:15:23 -0300

    Adjustment layout

--- src/components/overview/overview.vue
+++ src/components/overview/overview.vue
@@ -5,21 +5,21 @@
                         contentInfo: 'content-success',
                         title: 'Horas de música tocadas', 
                         info: cards.hours_music.hours + 'h',
-                        infoPerc: cards.hours_music.percent_diference}">
+                        infoPerc: cards.hours_music.percent_diference + '%'}">
         </OverviewItem>
         <OverviewItem 
             :parentData = "{ icon: 'icon-music',
                         contentInfo: 'content-success',
                         title: 'Quantidade de bandas de rock', 
                         info: cards.quantity_bands.bands,
-                        infoPerc: cards.quantity_bands.percent_diference}">
+                        infoPerc: cards.quantity_bands.percent_diference + '%'}">
         </OverviewItem>
         <OverviewItem 
             :parentData = "{ icon: 'icon-tomb',
                         contentInfo: 'content-danger',
                         title: 'Perdas precoces de artistas incríveis', 
                         info: cards.lost_artists.losts,
-                        infoPerc: cards.lost_artists.percent_diference}">
+                        infoPerc: cards.lost_artists.percent_diference + '%'}">
         </OverviewItem>
   </section>
 </template>
